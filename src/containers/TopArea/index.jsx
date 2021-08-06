b36d00eb79053708434d4001226d619e4a9d6ebe
Kalib Hackin <nagui08@hotmail.com>
2021-08-06 20:11:56 +0000

    creating top area

---
+++ src/containers/TopArea/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TopArea = () => (
+  <div className='top-area'>
+    <div className='text-area'>
+      <h1 className='title'>10,000+ of our users love our products.</h1>
+      <p className='legend'>We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.</p>
+    </div>
+    <div></div>
+  </div>
+);
+
+export default TopArea;
