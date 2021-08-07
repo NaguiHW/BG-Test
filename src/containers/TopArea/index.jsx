482d650f7090c0be225389b6ffd185df359b8cd0
Kalib Hackin <nagui08@hotmail.com>
2021-08-07 18:32:53 +0000

    styling finished to area

--- src/containers/TopArea/index.jsx
+++ src/containers/TopArea/index.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import RateCard from '../../components/RateCard';
+
+const rates = [
+  {
+    stars: 5,
+    text: 'Rated 5 Stars in Reviews',
+  },
+  {
+    stars: 5,
+    text: 'Rated 5 Stars in Report Guru',
+  },
+  {
+    stars: 5,
+    text: 'Rated 5 Stars in BestTech',
+  },
+]
 
 const TopArea = () => (
   <div className='top-area'>
-    <div className='text-area'>
+    <div className='text-container'>
       <h1 className='title'>10,000+ of our users love our products.</h1>
       <p className='legend'>We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.</p>
     </div>
-    <div></div>
+    <div className='rate-container'>
+      {
+        rates.map((rate, i) => (
+          <RateCard stars={rate.stars} text={rate.text} i={i} key={i} />
+        ))
+      }
+    </div>
   </div>
 );
 
